970531bbc6a3e4be73c1e71555c8b2743c06e7cc
Kevin <kjbrandstatter@gmail.com>
2012-03-01 22:18:48 -0600

    new file:   INSTALL 	modified:   README 	modified:   TODO 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,72 @@
 #!/usr/bin/env python
 
-import sys
+import sys, os
 from distutils.core import setup
-from distutils.extension import Extension
+from distutils.dist import Distribution
+from distutils.cmd import Command
+from distutils.command.install_data import install_data
+from distutils.command.build import build
+from distutils.dep_util import newer
+from distutils.log import warn, info, error
+from distutils.errors import DistutilsFileError
+
+class Uninstall(Command):
+  description = "Attempt an uninstall from an install --record file"
+
+  user_options = [('manifest=', None, 'Installation record filename')]
+
+  def initialize_options(self):
+    self.manifest = None
+
+  def finalize_options(self):
+    pass
+
+  def get_command_name(self):
+    return 'uninstall'
+
+  def run(self):
+    f = None
+    self.ensure_filename('manifest')
+    try:
+      try:
+        if not self.manifest:
+            raise DistutilsFileError("Pass manifest with --manifest=file")
+        f = open(self.manifest)
+        files = [file.strip() for file in f]
+      except IOError, e:
+        raise DistutilsFileError("unable to open install manifest: %s", 
+str(e))
+    finally:
+      if f:
+        f.close()
+
+    for file in files:
+      if os.path.isfile(file) or os.path.islink(file):
+        info("removing %s" % repr(file))
+        if not self.dry_run:
+          try:
+            os.unlink(file)
+          except OSError, e:
+            warn("could not delete: %s" % repr(file))
+      elif not os.path.isdir(file):
+        info("skipping %s" % repr(file))
+
+    dirs = set()
+    for file in reversed(sorted(files)):
+      dir = os.path.dirname(file)
+      if dir not in dirs and os.path.isdir(dir) and len(os.listdir(dir)) == 0:
+        dirs.add(dir)
+        # Only nuke empty Python library directories, else we could destroy
+        # e.g. locale directories we're the only app with a .mo installed for.
+        if dir.find("site-packages/") > 0:
+          info("removing %s" % repr(dir))
+          if not self.dry_run:
+            try:
+              os.rmdir(dir)
+            except OSError, e:
+              warn("could not remove directory: %s" % str(e))
+        else:
+          info("skipping empty directory %s" % repr(dir))
 
 setup(name='portupd', version='0.01',
       description='Portage tree auto sync daemon',
@@ -12,6 +76,7 @@ setup(name='portupd', version='0.01',
       scripts=['portupd'],
       data_files=[('/etc', ['portup.conf']),
          ('/etc/init.d', ['scripts/portupd'])],
-      requires=['portage', '_emerge']
+      requires=['portage', '_emerge'],
+      cmdclass = {'uninstall': Uninstall}
       )
 
