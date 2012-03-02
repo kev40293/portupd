44f170991a191fdf5b13f3094dabbb2c1c0f9e6a
Kevin <kjbrandstatter@gmail.com>
2012-03-01 21:58:27 -0600

    renamed:    initscript -> scripts/portupd 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python
+
+import sys
+from distutils.core import setup
+from distutils.extension import Extension
+
+setup(name='portupd', version='0.01',
+      description='Portage tree auto sync daemon',
+      author='Kevin Brandstatter',
+      author_email='Kevin Brandstatter',
+      packages=['portupdlib'], license="GPLv3",
+      scripts=['portupd'],
+      data_files=[('/etc', ['portup.conf']),
+         ('/etc/init.d', ['scripts/portupd'])],
+      requires=['portage', '_emerge']
+      )
+
